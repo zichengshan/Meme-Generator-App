798412234f5a9c4876e735d2f70fe0671447d442
Zicheng.S <zichengshan@gmail.com>
2022-03-18 17:47:59 -0700

    Get random meme from memesData

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,6 +1,15 @@
 import React from "react";
+import memesData from "../memesData";
 
 export default function Meme(){
+    let url
+    function getMemeImage(){
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        url  =memesArray[randomNumber].url
+        // const {url} = memesArray[randomNumber]
+        console.log(url)
+    }
     return (
         <main>
             <form className="form">
@@ -14,7 +23,12 @@ export default function Meme(){
                     placeholder="Bottom Text"
                     className="form--input"
                 />
-                <button className="form--button"> Get a new meme image ⛰️</button>
+                <button
+                    onClick={getMemeImage}
+                    className="form--button"
+                >
+                    Get a new meme image ⛰️
+                </button>
             </form>
         </main>
     )
